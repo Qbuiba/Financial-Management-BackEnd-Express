587643ff4b7e8c137e026a7401f34915f6453c5a
Qbuiba <37890918+Qbuiba@users.noreply.github.com>
2024-07-06 17:19:52 -0500

    update code to validating the username and password in registry

--- routes/auth.js
+++ routes/auth.js
@@ -6,11 +6,25 @@ const User = require('../models/User');
 
 const router = express.Router();
 
+
+// Helper function to validate username
+const validateUsername = (username) => {
+  const regex = /^[a-zA-Z0-9]{3,15}$/;
+  return regex.test(username);
+};
+
 //Register route
 router.post('/register', async (req, res) =>{
     const {username, password} = req.body;
 
+    if (!validateUsername(username)) {
+        return res.status(400).json({ message: 'Username must be 3-15 characters long and contain no special characters.' });
+    }
     
+      if (password.length < 6 || password.length > 8) {
+        return res.status(400).json({ message: 'Password must be between 6 and 8 characters.' });
+    }
+
     try{
         //Check if user existed in database -> Error -> To pick other username
         let user = await User.findOne({username});
